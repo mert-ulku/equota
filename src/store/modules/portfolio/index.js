a75df7236f82080277f766271eec3b877f402dea
mert ulku <mert.ulku@prodaft.com>
2022-10-28 19:23:06 +0300

    list component continued

--- src/store/modules/portfolio/index.js
+++ src/store/modules/portfolio/index.js
@@ -2,6 +2,7 @@ export default {
   state: {
     userData: [],
     portfolioData: [],
+    searchResults: []
   },
   getters: {
     getUserData(state) { 
@@ -9,6 +10,9 @@ export default {
     },
     getPortfolioData(state) { 
       return state.portfolioData;
+    },
+    getSearchResults(state) { 
+      return state.searchResults;
     }
   },
   mutations: {
@@ -17,6 +21,17 @@ export default {
     },
     setPortfolioData(state, payload) { 
       state.portfolioData = payload;
+    },
+    setSearchResults(state, payload) {
+      state.searchResults = payload;
+    },
+    addNewUserData(state, payload) {
+      state.userData = [...state.userData, payload];
+
+      const storedData = localStorage.getItem("previousData") 
+      let parsedData = (storedData && JSON.parse(storedData)) || []
+      parsedData = [...parsedData, payload]
+      localStorage.setItem("previousData", JSON.stringify(parsedData))
     }
   },
   actions: {
@@ -24,7 +39,24 @@ export default {
       commit('setUserData', userData);
     },
     setPortfolioData({ commit }, portfolioData) { 
-      commit('setPortfolioData', portfolioData);
+      commit('setPortfolioData', portfolioData.map(item => {
+        return {
+          ...item,
+          count: 0
+        }
+      }));
+    },
+    setSearchResults({ commit }, searchResults) { 
+      commit('setSearchResults', searchResults);
+    },
+    updateQuantity({state}, {value, symbol}) { 
+
+      const foundItem = state.searchResults.find(item => item.symbol === symbol);
+      foundItem.count = value;
+    },
+    addNewUserData({ commit }, userData) { 
+      console.log(userData)
+      commit('addNewUserData', userData);
     }
   },
   namespaced: true
