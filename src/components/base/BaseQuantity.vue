02d05a66bf64f8b6c4407f76b0cd519feafea27f
mert ulku <mert.ulku@prodaft.com>
2022-10-28 19:21:02 +0300

    quantity component add

---
+++ src/components/base/BaseQuantity.vue
@@ -0,0 +1,24 @@
+<template>
+  <number-input 
+    size="small"  
+    controls
+    :value="value"
+    center
+    inline
+    v-on="$listeners"
+    style="z-index: -1"
+  >
+  </number-input>
+</template>
+
+<script>
+  export default {
+    name: 'BaseQuantity',
+    props: {
+      value: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
